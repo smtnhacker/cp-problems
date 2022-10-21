9920370f811e1cc736c897f68a48ef452433b5e8
smtnhacker <themikosurara@gmail.com>
2022-10-21 09:53:12 +0800

    feat: add inefficient display name fetching

--- client/src/components/Posts/PostsView.tsx
+++ client/src/components/Posts/PostsView.tsx
@@ -1,19 +1,41 @@
+import { useState, useEffect } from 'react'
 import { Link } from "react-router-dom"
 import { useAppSelector } from "../../app/hooks"
 import { selectPosts } from "../../features/list/listSlice"
+import UserModel from '../../model/UserModel'
 
 const PostsView = () => {
     const posts = useAppSelector(selectPosts)
+    const sortedPosts = posts.slice().sort((a, b) => a.lastModified < b.lastModified ? -1 : 1)
+    const [authorCache, setAuthorCache] = useState({})
+
+    const getAuthorHandle = (authorID: string) => {
+        if (authorID in authorCache) {
+            return;
+        }
+        UserModel.fetchUserDetails(authorID)
+            .then(res => {
+                if (res.error) {
+                    console.log(res.error)
+                } else {
+                    setAuthorCache(prev => ({...prev, [authorID]: res.data.displayName}))
+                }
+            })
+    }
+
+    useEffect(() => {
+        posts.forEach(entry => getAuthorHandle(entry.authorID))
+    }, [])
 
     return (
        <>
             <h3>Recent Submissions</h3>
             <ul className="list-group">
-            {posts.map(entry => {
+            {sortedPosts.map(entry => {
                 return (
                 <li className="list-group-item" key={entry.id}>
                     <Link className="nav-link" to={`/posts/${entry.id || '404'}`}>
-                        {entry.title}
+                        {entry.title} by {authorCache[entry.authorID]}
                     </Link>
                 </li>
                 )
